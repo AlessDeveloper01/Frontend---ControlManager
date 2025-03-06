81388bee3b1d4fbdc2f257cbdee7c9631c9140b2
AlessDeveloper01 <alexbyoctubre@gmail.com>
2025-03-05 23:07:57 -0600

    Your commit message here

--- app/(main)/dashboard/layout.tsx
+++ app/(main)/dashboard/layout.tsx
@@ -13,50 +13,50 @@ const Footer = React.lazy(() => import('@/src/layouts/Footer'))
 
 const loading = () => <div />
 
-const validationAccountPersonal = async (router: ReturnType<typeof useRouter>) => {
-	const setUser = useAuth((state) => state.setUser);
-	const data = await validationToken();
-	if(data.errors && data.errors.length > 0) {
-		router.push('/auth/login');
-	}
+const validationAccountPersonal = async (router: ReturnType<typeof useRouter>, setUser: (data: any) => void) => {
+    const data = await validationToken();
+    if(data.errors && data.errors.length > 0) {
+        router.push('/auth/login');
+    }
 
-	setUser(data);
+    setUser(data);
 }
 
 export default function DashboardLayout({
  children
 }: {
  children: React.ReactNode;
-	}) {
-	const data = useAuth((state) => state.user);
-	const router = useRouter();
-	useEffect(() => {
-		validationAccountPersonal(router);
-	}, [router]);
+    }) {
+    const data = useAuth((state) => state.user);
+    const setUser = useAuth((state) => state.setUser);
+    const router = useRouter();
+    useEffect(() => {
+        validationAccountPersonal(router, setUser);
+    }, [router, setUser]);
 
-	return (
+    return (
     <>
             <Suspense fallback={loading()}>
-				<div className="flex wrapper">
-					{
-						<Suspense fallback={loading()}>
-							<LeftSideBar hideUserProfile={true} isCondensed={true} isLight={false} data={data} />
-						</Suspense>
-					}
+                <div className="flex wrapper">
+                    {
+                        <Suspense fallback={loading()}>
+                            <LeftSideBar hideUserProfile={true} isCondensed={true} isLight={false} data={data} />
+                        </Suspense>
+                    }
 
-					<div className="page-content">
-						<Suspense fallback={loading()}>
-							<Topbar />
-						</Suspense>
+                    <div className="page-content">
+                        <Suspense fallback={loading()}>
+                            <Topbar />
+                        </Suspense>
 
-						<main className="p-6 overflow-x-hidden">
-							<Suspense fallback={<Preloader />}>{children}</Suspense>
-						</main>
+                        <main className="p-6 overflow-x-hidden">
+                            <Suspense fallback={<Preloader />}>{children}</Suspense>
+                        </main>
 
-						<Footer />
-					</div>
-				</div>
-			</Suspense>
-		</>
+                        <Footer />
+                    </div>
+                </div>
+            </Suspense>
+        </>
   );
 }
