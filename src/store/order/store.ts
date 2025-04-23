2764ab9d619a0158edc3fcd1ed6ac7721e436cd5
AlessDeveloper01 <alexbyoctubre@gmail.com>
2025-04-23 14:52:30 -0600

    🐼Add Change Logo

--- src/store/order/store.ts
+++ src/store/order/store.ts
@@ -1,29 +1,44 @@
 import { OrderItemIndividualType, OrderItemMenuType } from "@/src/Objects";
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 
 export type OrderStore = {
-    orders: OrderItemMenuType,
-    setOrders: (orders: OrderItemMenuType) => void
-    order: OrderItemIndividualType,
-    setOrder: (order: OrderItemIndividualType) => void
-    id: number,
-    setId: (id: number) => void
-    payment: string,
-    setPayment: (payment: string) => void
-    page: number,
-    setPage: (page: number) => void
-}
+    orders: OrderItemMenuType;
+    setOrders: (orders: OrderItemMenuType) => void;
+    order: OrderItemIndividualType;
+    setOrder: (order: OrderItemIndividualType) => void;
+    id: number;
+    setId: (id: number) => void;
+    payment: string;
+    setPayment: (payment: string) => void;
+    page: number;
+    setPage: (page: number) => void;
+};
 
-export const useOrder = create<OrderStore>()(devtools((set) => ({
-    orders: { orders: [], totalPages: 0 } as unknown as OrderItemMenuType,
-    setOrders: (orders) => set({ orders }),
-    order: {} as OrderItemIndividualType,
-    setOrder: (order) => set({ order }),
-    id: 0,
-    setId: (id) => set({ id }),
-    payment: '',
-    setPayment: (payment) => set({ payment }),
-    page: 1,
-    setPage: (page) => set({ page })
-})))
+export const useOrder = create<OrderStore>()(
+    devtools(
+        persist(
+            (set) => ({
+                orders: {
+                    orders: [],
+                    totalPages: 0,
+                } as unknown as OrderItemMenuType,
+                setOrders: (orders) => set({ orders }),
+                order: {} as OrderItemIndividualType,
+                setOrder: (order) => set({ order }),
+                id: 0,
+                setId: (id) => set({ id }),
+                payment: "",
+                setPayment: (payment) => set({ payment }),
+                page: 1,
+                setPage: (page) => set({ page }),
+            }),
+            {
+                name: "order-storage",
+                partialize: (state) => ({
+                    order: state.order,
+                })
+            }
+        )
+    )
+);
